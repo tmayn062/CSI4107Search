e40168c4bfa54445c11c76fb08c364f58e8a47f6
Jonathan PC <boerger.jonathan@gmail.com>
2020-02-06 22:10:14 -0500

    bollean query expansion (* and LPP)

--- boolean_query_parser.py
+++ boolean_query_parser.py
@@ -18,9 +18,10 @@ Description:
 from pythonds.basic import Stack
 
 from wildcard_management import wildcard_word_finder
+from linguistic_processor import linguistic_module
 
 """
- Next steps:
+ Next steps:(done +/-)
  -> expand wildcards in search prior to converting to postfix
         |-> replace word with * with expansion
                 |-> could be as simple as .replace or query[xx:yy] 
@@ -39,11 +40,36 @@ from wildcard_management import wildcard_word_finder
 
 
 def main():
-    test_string=wildcard_word_finder('*ge', "uottawa_bigraph_index.csv")
-    test_string=test_string.replace('(', '( ')
-    test_string =test_string.replace(')', ' )')
+    # boolean query linguistic pre-processing and wildcard expansion
+    linguistic_processing_parameters = {"do_contractions": True, "do_normalize_hyphens": True,
+                                        "do_normalize_periods": True, "do_remove_punctuation": True,
+                                        "do_case_fold": True, "do_stop_word_removal": True,
+                                        "do_stemming": True, "do_lemming": False}
+    exclusion = ["AND", "OR", "AND_NOT", '(',')']
+    test_query="(*ge AND_NOT (man* OR health*))"
+    test_query = test_query.replace('(', '( ')
+    test_query = test_query.replace(')', ' )')
+    test_query_list=test_query.split()
+    print(test_query_list)
+    output_list=[]
+    for elements in test_query_list:
+        if elements not in exclusion:
+            elements=linguistic_module(elements, linguistic_processing_parameters)
+            if elements[0].find("*") != -1:
+                elements=wildcard_word_finder(elements[0], "uottawa_bigraph_index.csv")
+        output_list.append(elements)
+    print(output_list)
+    test_string=" ".join(output_list)
+    print(test_string)
+
+
+
+
+    #test_string=wildcard_word_finder('*ge', "uottawa_bigraph_index.csv")
+    # test_string=test_string.replace('(', '( ')
+    # test_string =test_string.replace(')', ' )')
     test_string=test_string.split()
-    operators=["AND","OR","NOT_AND"]
+    operators=["AND","OR","AND_NOT"]
     opStack = Stack()
     postfixList = []
     for token in test_string:
--- wildcard_management.py
+++ wildcard_management.py
@@ -85,11 +85,12 @@ def wildcard_word_finder(wildcard_search_word, bigraph_index_filename):
     # can be used in a boolean search
     if len(actual_word_list) == 0:
         return ""
-    or_string = f'({actual_word_list[0]}'
+    or_string = f'{actual_word_list[0]}'
     if len(actual_word_list) > 1:
+        or_string='( '+or_string
         for x in range(1, len(actual_word_list)):
             or_string = or_string + f' OR {actual_word_list[x]}'
-    or_string = or_string + ')'
+        or_string = or_string + ' )'
 
     return or_string
 
